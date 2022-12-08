0c7fcae7d0d44855e8bd0aa64d89d8f521b936dc
JhonatanWCL99 <wilsoncoyoleon@gmail.com>
2022-12-08 14:45:25 -0400

    with url server railway

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/room";
+const API_URL = "https://c4diagrammerbackend.up.railway.app/api/room";
+//const API_URL = "http://localhost:4000/api/room";
 
 class RoomService {
     allRoomsAnfitrion(idAnfitrion) {
